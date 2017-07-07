35c2f7c382d60db0082ea4cd566ca392aa07df79
RaviKrishnaPatloori <ravi.krishna@accionlabs.com>
2017-07-07 17:12:35 +0530

    Update Encryptor.cs

--- MTTLEncryptorForm/MttlEncryptor/Encryptor.cs
+++ MTTLEncryptorForm/MttlEncryptor/Encryptor.cs
@@ -25,9 +25,7 @@ namespace MttlEncryptor
                     using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
                     {
                         cStream.Write(bytesBuff, 0, bytesBuff.Length);
-                        cStream.Close();
-						//Comment1
-						//Comment2
+                        cStream.Close();						
                     }					
                     inText = Convert.ToBase64String(mStream.ToArray());
                 }
